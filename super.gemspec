c12e7702d490df9d6ebbdd2604d7699b0fc046d1
Zach Ahn <engineering@zachahn.com>
2019-05-02 20:41:45 -0400

    Install and setup Capybara

--- super.gemspec
+++ super.gemspec
@@ -16,6 +16,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "rails", "~> 5.2.2"
 
+  spec.add_development_dependency "capybara"
   spec.add_development_dependency "sqlite3", "~> 1.3.6"
   spec.add_development_dependency "pry"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,11 +6,13 @@ require "pry"
 require_relative "../test/dummy/config/environment"
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 require "rails/test_help"
+require "capybara/rails"
+require "capybara/dsl"
+require "capybara/minitest"
 
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
-
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
@@ -18,4 +20,14 @@ if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
   ActiveSupport::TestCase.fixtures :all
+end
+
+class CapybaraTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  include Capybara::Minitest::Assertions
+
+  teardown do
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
 end
