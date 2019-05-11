85d46b3e7665ff9163fa79ce435ddea2bb9ad55d
Zach Ahn <engineering@zachahn.com>
2019-05-10 23:18:50 -0400

    Setup Rails to use assets managed by Parcel
    I've set up the dummy application to use a special library-development
    asset build. I plan on committing the compiled assets for ease of use.
    (See parent commit for some related changes, namely `package.json`)
    
    In addition:
    
    * Remove vendored assets (I've already moved it to Parcel)
    * Add compatibility with Sprockets 3 and 4

--- lib/super/engine.rb
+++ lib/super/engine.rb
@@ -1,4 +1,9 @@
 module Super
   class Engine < ::Rails::Engine
+    initializer "super.assets.precompile" do |app|
+      if [:sprockets3, :sprockets4].include?(Super.environment.assets_via)
+        app.config.assets.precompile << "config/super_manifest.js"
+      end
+    end
   end
 end
--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -52,6 +52,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Use library development assets built by Parcel (Don't add this to real apps!)
+  config.assets.paths << File.expand_path("../../../../tmp/frontend_dev_dist/frontend", __dir__)
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
