22d0b1d8cfeb8cdce81493795b533579ee772f6a
Zach Ahn <engineering@zachahn.com>
2019-07-25 22:14:12 -0400

    Update `super:resource` generator
    It had gotten a bit out of date. The generator also now uses the
    configuration set in the Super initializer

--- lib/generators/super/resource/resource_generator.rb
+++ lib/generators/super/resource/resource_generator.rb
@@ -3,11 +3,52 @@ module Super
     source_root File.expand_path("templates", __dir__)
 
     def create_controller
-      template("resources_controller.rb", "app/controllers/admin/#{file_path.pluralize}_controller.rb")
+      controller_subdir =
+        if Super.configuration.controller_namespace.present?
+          "#{Super.configuration.controller_namespace}/"
+        else
+          ""
+        end
+
+      template(
+        "resources_controller.rb",
+        "app/controllers/#{controller_subdir}#{file_path.pluralize}_controller.rb"
+      )
+
+      template("resource_dashboard.rb", "app/super/#{file_path}_dashboard.rb")
     end
 
-    def create_controls
-      template("resource_controls.rb", "app/controls/#{file_path}_controls.rb")
+    private
+
+    def wrap_with_configured_module(&block)
+      indentation_amount =
+        if Super.configuration.controller_namespace.present?
+          2
+        else
+          0
+        end
+
+      inner = capture(&block)
+      inner = indent(inner, indentation_amount).chomp
+
+      if Super.configuration.controller_namespace.present?
+        concat("module #{Super.configuration.controller_namespace.camelize}\n")
+      end
+
+      concat(inner)
+      concat("\n")
+
+      if Super.configuration.controller_namespace.present?
+        concat("end\n")
+      end
+    end
+
+    def parent_controller_name
+      if Super.configuration.controller_namespace.present?
+        "#{Super.configuration.controller_namespace.camelize}Controller"
+      else
+        "AdminController"
+      end
     end
   end
 end
--- test/lib/generators/super/resource_generator_test.rb
+++ test/lib/generators/super/resource_generator_test.rb
@@ -7,9 +7,124 @@ class Super::ResourceGeneratorTest < Rails::Generators::TestCase
   setup :prepare_destination
 
   def test_generator_creates_expected_files
+    @original_configuration = Super.configuration
+    Super.instance_variable_set(:@configuration, nil)
+    Super.configuration do |c|
+      c.controller_namespace = "badminton"
+    end
+
     run_generator(["ship"])
 
-    assert_file("app/controllers/admin/ships_controller.rb")
-    assert_file("app/controls/ship_controls.rb")
+    assert_file("app/controllers/badminton/ships_controller.rb", <<~RUBY)
+      module Badminton
+        class ShipsController < BadmintonController
+          private
+
+          def dashboard
+            ShipDashboard.new
+          end
+        end
+      end
+    RUBY
+
+    assert_file("app/super/ship_dashboard.rb")
+  ensure
+    Super.instance_variable_set(:@configuration, @original_configuration)
+  end
+
+  def test_generator_creates_expected_files_when_blank_namespace
+    @original_configuration = Super.configuration
+    Super.instance_variable_set(:@configuration, nil)
+    Super.configuration do |c|
+      c.controller_namespace = ""
+    end
+
+    run_generator(["ship"])
+
+    assert_file("app/controllers/ships_controller.rb", <<~RUBY)
+      class ShipsController < AdminController
+        private
+
+        def dashboard
+          ShipDashboard.new
+        end
+      end
+    RUBY
+
+    assert_file("app/super/ship_dashboard.rb")
+  ensure
+    Super.instance_variable_set(:@configuration, @original_configuration)
+  end
+
+  def test_dashboard_looks_right
+    run_generator(["ship"])
+
+    assert_file("app/super/ship_dashboard.rb", <<~RUBY)
+      class ShipDashboard
+        def title
+          Ship.name.pluralize
+        end
+
+        def model
+          Ship
+        end
+
+        def index_scope
+          Ship.all
+        end
+
+        def index_columns
+          [
+          ]
+        end
+
+        def create_scope
+          Ship.all
+        end
+
+        def new_scope
+          Ship.all
+        end
+
+        def new_columns
+          [
+          ]
+        end
+
+        def edit_scope
+          Ship.all
+        end
+
+        def edit_columns
+          [
+          ]
+        end
+
+        def show_scope
+          Ship.all
+        end
+
+        def show_columns
+          [
+          ]
+        end
+
+        def update_scope
+          Ship.all
+        end
+
+        def destroy_scope
+          Ship.all
+        end
+
+        def permitted_create_params(params)
+          params.require(:ship).permit()
+        end
+
+        def permitted_update_params(params)
+          params.require(:ship).permit()
+        end
+      end
+    RUBY
   end
 end
