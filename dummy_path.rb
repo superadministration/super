2cc03be2f4eca56c823fad87539b7e77dd9fb544
Zach Ahn <engineering@zachahn.com>
2019-11-28 22:24:04 -0500

    Add script to do things for each dummy

--- dummy_path.rb
+++ dummy_path.rb
@@ -1,10 +1,15 @@
-SUPER_DUMMY_PATH =
-  case ENV["BUNDLE_GEMFILE"]
-  when /rails50_sprockets3/ then "test/dummy50_sprockets3"
-  when /rails51_sprockets3/ then "test/dummy51_sprockets3"
-  when /rails52_sprockets4/ then "test/dummy52_sprockets4"
-  when /rails60_sprockets4/ then "test/dummy60_sprockets4"
-  when /rails60_webpacker4/ then "test/dummy60_webpacker4"
-  else
-    "test/dummy60_sprockets4"
+SUPER_DEFAULT_RAILS = "rails60_sprockets4"
+SUPER_RAILS_ROOTS = {
+  "rails50_sprockets3" => "test/dummy50_sprockets3",
+  "rails51_sprockets3" => "test/dummy51_sprockets3",
+  "rails52_sprockets4" => "test/dummy52_sprockets4",
+  "rails60_sprockets4" => "test/dummy60_sprockets4",
+  "rails60_webpacker4" => "test/dummy60_webpacker4",
+}
+
+query =
+  if ENV["BUNDLE_GEMFILE"]
+    File.basename(ENV["BUNDLE_GEMFILE"], ".gemfile")
   end
+
+SUPER_DUMMY_PATH = SUPER_RAILS_ROOTS[query] || SUPER_RAILS_ROOTS.fetch(SUPER_DEFAULT_RAILS)
