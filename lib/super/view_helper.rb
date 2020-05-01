867009e98b9b7d03f079d670ad919bfe07a40036
Zach Ahn <engineering@zachahn.com>
2020-05-01 07:58:46 -0400

    Show form label conditionally

--- lib/super/view_helper.rb
+++ lib/super/view_helper.rb
@@ -2,6 +2,25 @@ module Super
   module ViewHelper
     module_function
 
+    # For example, calling `classes("always", ["sometimes", condition])` would
+    # return the string "always sometimes" or "always"
+    def classes(*list)
+      result = list.map do |c|
+        case c
+        when String
+          c
+        when Array
+          if c.size != 2
+            raise %(Expected exactly two elements (["class", some_condition]), got: #{c.inspect})
+          end
+
+          c if c.last
+        end
+      end
+
+      result.compact.join(" ")
+    end
+
     def errors_accounting_for_reflections(model_instance, column_or_association)
       errable_fields(model_instance, column_or_association)
         .flat_map { |field| Compatability.rails_50_errable_fields(field) }
