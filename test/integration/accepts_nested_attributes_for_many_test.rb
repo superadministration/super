77a683345e2172ef62a311ed235c6e3b6625fe10
Zach Ahn <engineering@zachahn.com>
2020-05-21 11:01:43 -0400

    Add test for nested attributes

---
+++ test/integration/accepts_nested_attributes_for_many_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class AcceptsNestedAttributesForManyTest < CapybaraTest
+  chrome!
+
+  def test_it_works
+    visit(edit_admin_member_path(members(:riker)))
+
+    click_on("Add Favorite thing")
+
+    within_fieldset("Favorite thing") do
+      fill_in("Name", with: "Trumpet")
+    end
+
+    assert_difference(-> { members(:riker).reload.favorite_things.size }, 1) do
+      click_on("Update Member")
+    end
+
+    assert_equal(admin_member_path(members(:riker)), page.current_path)
+    assert(page.has_content?("Trumpet"))
+  end
+end
