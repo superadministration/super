d8c0f3b0588f38307b3e280c42bfa48f8e8f48ee
Zach Ahn <engineering@zachahn.com>
2020-12-28 22:56:48 -0500

    Fix local webpacker tests
    CI regenerates the entire Rails app which is why these tests looked
    like they were passing

--- test/dummy/rails60_webpacker5/config/initializers/super.rb
+++ test/dummy/rails60_webpacker5/config/initializers/super.rb
@@ -2,5 +2,6 @@ Super.configuration do |c|
   c.title = "My Admin Site"
   c.controller_namespace = "admin"
   c.route_namespace = "admin"
-  c.asset_handler = Super::Assets.webpacker
+  c.javascripts = Super::Assets.use_webpacker(c.javascripts)
+  c.stylesheets = Super::Assets.use_webpacker(c.stylesheets)
 end
