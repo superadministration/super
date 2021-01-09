4b7bbdacc32ffec6dae45fb7d7a727da5d909a1d
Zach Ahn <engineering@zachahn.com>
2021-01-09 17:22:43 -0500

    Move "inline errors" methods

--- lib/super/view_helper.rb
+++ lib/super/view_helper.rb
@@ -20,24 +20,5 @@ module Super
 
       result.compact.join(" ")
     end
-
-    def errors_accounting_for_reflections(model_instance, column_or_association)
-      errable_fields(model_instance, column_or_association)
-        .flat_map { |field| Compatability.errable_fields(field) }
-        .flat_map { |field| model_instance.errors.full_messages_for(field) }
-        .uniq
-    end
-
-    def errable_fields(model_instance, column_or_association)
-      column_or_association = column_or_association.to_s
-      reflection = model_instance.class.reflect_on_association(column_or_association)
-      reflection ||= model_instance.class.reflections.values.find { |r| r.foreign_key == column_or_association }
-
-      if reflection
-        [reflection.name.to_s, reflection.foreign_key.to_s]
-      else
-        [column_or_association]
-      end
-    end
   end
 end
