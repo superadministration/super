3c3a9c99f723c0ddd4805746665aa16526f1a46f
Zach Ahn <engineering@zachahn.com>
2021-04-26 21:21:11 -0400

    Allow builder methods to accept arguments

--- lib/super/useful/builder.rb
+++ lib/super/useful/builder.rb
@@ -6,8 +6,8 @@ module Super
       def builder(method_name)
         alias_method("original_#{method_name}", method_name)
 
-        define_method(method_name) do
-          send("original_#{method_name}")
+        define_method(method_name) do |*args|
+          send("original_#{method_name}", *args)
           self
         end
       end
@@ -15,8 +15,8 @@ module Super
       def builder_with_block(method_name)
         alias_method("original_#{method_name}", method_name)
 
-        define_method(method_name) do |&block|
-          send("original_#{method_name}", &block)
+        define_method(method_name) do |*args, &block|
+          send("original_#{method_name}", *args, &block)
           self
         end
       end
