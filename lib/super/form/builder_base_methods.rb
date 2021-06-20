b62e881e6b1524cbbefb05f9a97fb2b16acfd2be
Zach Ahn <engineering@zachahn.com>
2021-06-20 15:57:57 +0900

    Add many more form field helpers
    I copied the list of methods to skip from Rails
    
    * lib/action_view/helpers/form_helper.rb
    * From commit cde84b85eb76f95a661860f06f86aae5a12ccac9

--- lib/super/form/builder_base_methods.rb
+++ lib/super/form/builder_base_methods.rb
@@ -4,6 +4,30 @@ module Super
   class Form
     class Builder
       class Wrappers
+        skipped_field_helpers = [:label, :check_box, :radio_button, :fields_for, :fields, :hidden_field, :file_field]
+        (ActionView::Helpers::FormBuilder.field_helpers - skipped_field_helpers).each do |builder_method_name|
+          class_eval(<<~RUBY, __FILE__, __LINE__ + 1)
+            def #{builder_method_name}(attribute, options = {})
+              options, defaults = split_defaults(options, class: "super-input w-full")
+              options[:class] = join_classes(defaults[:class], options[:class])
+
+              @builder.#{builder_method_name}(attribute, options)
+            end
+
+            def #{builder_method_name}!(attribute, label_text: nil, label: {}, field: {}, show_errors: true)
+              container do
+                compact_join([
+                  public_send(:label, attribute, label_text, label),
+                  %(<div class="mt-1">).html_safe,
+                  public_send(:#{builder_method_name}, attribute, field),
+                  show_errors && inline_errors(attribute),
+                  %(</div>).html_safe,
+                ])
+              end
+            end
+          RUBY
+        end
+
         def label(attribute, text = nil, options = {}, &block)
           options, defaults = split_defaults(options, class: "block")
           options[:class] = join_classes(defaults[:class], options[:class])
@@ -29,50 +53,12 @@ module Super
           end
         end
 
-        def password_field(attribute, options = {})
-          options, defaults = split_defaults(options, class: "super-input w-full")
-          options[:class] = join_classes(defaults[:class], options[:class])
-
-          @builder.password_field(attribute, options)
-        end
-
-        def password_field!(attribute, label_text: nil, label: {}, field: {}, show_errors: true)
-          container do
-            compact_join([
-              public_send(:label, attribute, label_text, label),
-              %(<div class="mt-1">).html_safe,
-              public_send(:password_field, attribute, field),
-              show_errors && inline_errors(attribute),
-              %(</div>).html_safe,
-            ])
-          end
-        end
-
         def submit(value = nil, options = {})
           value, options = nil, value if value.is_a?(Hash)
           options, defaults = split_defaults(options, class: "super-button")
           options[:class] = join_classes(defaults[:class], options[:class])
 
           @builder.submit(value, options)
-        end
-
-        def text_field(attribute, options = {})
-          options, defaults = split_defaults(options, class: "super-input w-full")
-          options[:class] = join_classes(defaults[:class], options[:class])
-
-          @builder.text_field(attribute, options)
-        end
-
-        def text_field!(attribute, label_text: nil, label: {}, field: {}, show_errors: true)
-          container do
-            compact_join([
-              public_send(:label, attribute, label_text, label),
-              %(<div class="mt-1">).html_safe,
-              public_send(:text_field, attribute, field),
-              show_errors && inline_errors(attribute),
-              %(</div>).html_safe,
-            ])
-          end
         end
       end
     end
