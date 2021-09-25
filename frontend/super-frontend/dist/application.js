8109f29a628f4662d98024b5d7f0ef14a8643a78
Zach Ahn <engineering@zachahn.com>
2021-09-25 11:07:48 -0400

    Update all frontend dependencies

--- frontend/super-frontend/dist/application.js
+++ frontend/super-frontend/dist/application.js
@@ -252,8 +252,8 @@ var Super = (function (exports) {
               exclude: 'form button'
             },
             inputChangeSelector: 'select[data-remote], input[data-remote], textarea[data-remote]',
-            formSubmitSelector: 'form',
-            formInputClickSelector: 'form input[type=submit], form input[type=image], form button[type=submit], form button:not([type]), input[type=submit][form], input[type=image][form], button[type=submit][form], button[form]:not([type])',
+            formSubmitSelector: 'form:not([data-turbo=true])',
+            formInputClickSelector: 'form:not([data-turbo=true]) input[type=submit], form:not([data-turbo=true]) input[type=image], form:not([data-turbo=true]) button[type=submit], form:not([data-turbo=true]) button:not([type]), input[type=submit][form], input[type=image][form], button[type=submit][form], button[form]:not([type])',
             formDisableSelector: 'input[data-disable-with]:enabled, button[data-disable-with]:enabled, textarea[data-disable-with]:enabled, input[data-disable]:enabled, button[data-disable]:enabled, textarea[data-disable]:enabled',
             formEnableSelector: 'input[data-disable-with]:disabled, button[data-disable-with]:disabled, textarea[data-disable-with]:disabled, input[data-disable]:disabled, button[data-disable]:disabled, textarea[data-disable]:disabled',
             fileInputSelector: 'input[name][type=file]:not([disabled])',
