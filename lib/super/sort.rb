78ac47537a871313c5173b19766c4095de842ff4
Zach Ahn <engineering@zachahn.com>
2022-09-07 23:33:30 -0400

    Strictly type Super::Sort

--- lib/super/sort.rb
+++ lib/super/sort.rb
@@ -1,63 +1,90 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module Super
   module Sort
     class FormObject
+      extend T::Sig
+
       include Query::FormObjectInterface
 
-      DIRECTIONS = ["asc", "desc"]
+      DIRECTIONS = T.let(["asc", "desc"], T::Array[String])
 
+      sig do
+        params(
+          model: T.class_of(ActiveRecord::Base),
+          params: ActiveSupport::HashWithIndifferentAccess,
+          default: T::Hash[T.any(String, Symbol), T.any(String, Symbol)],
+          sortable_columns: T::Array[T.any(String, Symbol)]
+        ).void
+      end
       def initialize(model:, params:, default:, sortable_columns:)
         @model = model
         @params = params
         @default = default
-        @sortable_columns = sortable_columns.map(&:to_s)
+        @sortable_columns = T.let(sortable_columns.map(&:to_s), T::Array[String])
       end
 
+      sig { returns(T::Array[String]) }
       attr_reader :sortable_columns
 
+      sig { override.returns(String) }
       def to_partial_path
         "sort"
       end
 
+      sig { returns(T::Array[Super::Sort::FormObject::Expression]) }
       def exprs
-        sanitized_expr_params.map { |ordering| Expression.new(**ordering) }
+        sanitized_expr_params.map { |ordering| T.unsafe(Expression).new(**ordering) }
       end
 
+      sig { returns(Super::Sort::FormObject::Expression) }
       def new_expr
         Expression.new(a: nil, d: :desc)
       end
 
       class Expression
+        extend T::Sig
+
+        sig { returns(T.nilable(String)) }
         attr_accessor :a # attribute
+        sig { returns(String) }
         attr_accessor :d # direction
 
+        sig { params(a: T.nilable(String), d: T.nilable(T.any(String, Symbol))).void }
         def initialize(a:, d:)
-          @a = a
-          @d = d.to_s
+          @a = T.let(a, T.nilable(String))
+          @d = T.let(d.to_s, String)
+          @index = T.let(nil, T.nilable(T.any(Integer, String)))
         end
 
+        sig { params(index: T.any(Integer, String)).returns(Super::Sort::FormObject::Expression) }
         def with_index(index)
           @index = index
           self
         end
 
         # This is for `fields_for` to work
+        sig { returns(T.any(Integer, String)) }
         def id
-          @index
+          T.must(@index)
         end
 
+        sig { returns(T::Boolean) }
         def persisted?
           false
         end
       end
 
+      sig { returns(T::Boolean) }
       def persisted?
         false
       end
 
-      def apply_changes(query)
+      sig { override.params(relation: ActiveRecord::Relation).returns(ActiveRecord::Relation) }
+      def apply_changes(relation)
+        query = T.let(relation, ActiveRecord::Relation)
+
         exprs.each do |expr|
           query = query.order(expr.a => expr.d)
         end
@@ -68,12 +95,15 @@ module Super
       private
 
       # Stringifies the values
+      sig { returns(T::Array[T::Hash[Symbol, String]]) }
       def sanitized_expr_params
         seen = {}
         normalized_expr_params
-          .each do |p|
-            p[:a] = p[:a].presence&.to_s&.strip
-            p[:d] = p[:d].presence&.to_s&.strip
+          .map do |p|
+            {
+              a: p[:a].presence&.to_s&.strip,
+              d: p[:d].presence&.to_s&.strip
+            }
           end
           .select { |p| p[:a].present? && DIRECTIONS.include?(p[:d]) }
           .select { |p| @sortable_columns.include?(p[:a]) }
@@ -86,6 +116,7 @@ module Super
       end
 
       # Symbolizes the keys
+      sig { returns(T::Array[T::Hash[Symbol, Symbol]]) }
       def normalized_expr_params
         from_params = @params[:exprs]
 
@@ -98,6 +129,7 @@ module Super
         from_params.to_h.values.map(&:symbolize_keys)
       end
 
+      sig { returns(T::Array[T::Hash[Symbol, Symbol]]) }
       def default_as_query
         @default.map do |attribute_name, direction|
           {
